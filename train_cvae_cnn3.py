b95470b52a0ef59a90910ac0c4658443c1b7825c
asahi417 <spacefunkspacefunk@gmail.com>
2017-10-30 18:46:51 +0900

    train

--- train_cvae_cnn3.py
+++ train_cvae_cnn3.py
@@ -7,8 +7,13 @@ if __name__ == '__main__':
     # Ignore warning message by tensor flow
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-    save_path = "./log/cvae_cnn3/"
-    network_architecture = dict(n_input=[28, 28, 1], n_z=20)
+    # save_path = "./log/cvae_cnn3/"
+    # network_architecture = dict(n_input=[28, 28, 1], n_z=20)
+    # model = CVAE_CNN3(10, network_architecture=network_architecture, batch_size=100, save_path=save_path,
+    #                   learning_rate=0.005, activation=tf.nn.relu)
+
+    save_path = "./log/cvae_cnn3_2d/"
+    network_architecture = dict(n_input=[28, 28, 1], n_z=2)
     model = CVAE_CNN3(10, network_architecture=network_architecture, batch_size=100, save_path=save_path,
                       learning_rate=0.005, activation=tf.nn.relu)
-    mnist_train(model=model, epoch=300, save_path=save_path, mode="conditional", input_image=True)
+    mnist_train(model=model, epoch=100, save_path=save_path, mode="conditional", input_image=True)
