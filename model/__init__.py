03b6515ab9a8f6f909274609c79bbec9730d0aca
Asahi Ushio <spacefunkspacefunk@gmail.com>
2017-10-31 15:38:19 +0900

    newmodel

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
 from .cvae_cnn3 import ConditionalVAE as CVAE_CNN3
+from .cvae_cnn3_02 import ConditionalVAE as CVAE_CNN3_ver2
 from .cvae_cnn2 import ConditionalVAE as CVAE_CNN2
 from .vae import VariationalAutoencoder
 from .cnn import CNN
