cc9da7b8c579169fda9f5715b58a1fd611d2e38b
asahi417 <spacefunkspacefunk@gmail.com>
2017-11-03 22:52:00 +0900

    add cnn model

--- model/cnn.py
+++ model/cnn.py
@@ -24,33 +24,23 @@ def full_connected(x, weight_shape, initializer):
 class CNN(object):
     """ CNN """
 
-    def __init__(self, activation=tf.nn.relu, learning_rate=0.001, batch_size=100, save_path=None, load_model=None,
-                 keep_prob=0.8, architecture=None):
+    def __init__(self, network_architecture, activation=tf.nn.relu, learning_rate=0.001, batch_size=100, save_path=None,
+                 load_model=None, keep_prob=0.8, max_grad_norm=1):
         """
-        :param architecture:
+        :param network_architecture:
         :param activation: activation function (tensor flow function)
         :param float learning_rate:
         :param int batch_size:
         """
 
         self.activation = activation
+        self.max_grad_norm = max_grad_norm
         self.learning_rate = learning_rate
         self.keep_prob = keep_prob
         self.batch_size = batch_size
-        if architecture:
-            self.architecture = architecture
-        else:
-            self.architecture = {
-                "output": 10, "input": [28, 28, 1],
-                "filter1": [4, 4, 1, 8], "stride1": [1, 1],
-                "filter2": [4, 4, 8, 16], "stride2": [1, 1],
-                "filter3": [4, 4, 16, 32], "stride3": [1, 1],
-                "filter4": [4, 4, 32, 64], "stride4": [1, 1]
-            }
+        self.network_architecture = network_architecture
         # Initializer
         if "relu" in self.activation.__name__:
-            # # print("relu")
-            # tf.truncated_normal(shape, stddev=0.1)
             self.ini_c, self.ini = variance_scaling_initializer(), variance_scaling_initializer()
         else:
             # # print("not relu")
@@ -77,36 +67,36 @@ class CNN(object):
         """ Create Network, Define Loss Function and Optimizer """
         # tf Graph input
         _in_size = [None]
-        _in_size += self.architecture["input"]
+        _in_size += self.network_architecture["input"]
         self.x = tf.placeholder(tf.float32, _in_size, name="input")
-        self.y = tf.placeholder(tf.float32, [None, self.architecture["output"]], name="output")
+        self.y = tf.placeholder(tf.float32, [None, self.network_architecture["output"]], name="output")
         self.is_training = tf.placeholder(tf.bool)
         _keep_prob = self.keep_prob if self.is_training is True else 1
 
         # print(self.x.shape, self.y.shape)
-        _layer = convolution(self.x, self.architecture["filter1"], self.architecture["stride1"], self.ini_c)
+        _layer = convolution(self.x, self.network_architecture["filter1"], self.network_architecture["stride1"], self.ini_c)
         _layer = tf.nn.max_pool(_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         _layer = self.activation(_layer)
         _layer = tf.nn.dropout(_layer, _keep_prob)
         # print(_layer.shape)
-        _layer = convolution(_layer, self.architecture["filter2"], self.architecture["stride2"], self.ini_c)
+        _layer = convolution(_layer, self.network_architecture["filter2"], self.network_architecture["stride2"], self.ini_c)
         _layer = tf.nn.max_pool(_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         _layer = self.activation(_layer)
         _layer = tf.nn.dropout(_layer, _keep_prob)
         # print(_layer.shape)
-        _layer = convolution(_layer, self.architecture["filter3"], self.architecture["stride3"], self.ini_c)
+        _layer = convolution(_layer, self.network_architecture["filter3"], self.network_architecture["stride3"], self.ini_c)
         _layer = tf.nn.max_pool(_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         _layer = self.activation(_layer)
         _layer = tf.nn.dropout(_layer, _keep_prob)
         # print(_layer.shape)
-        _layer = convolution(_layer, self.architecture["filter4"], self.architecture["stride4"], self.ini_c)
+        _layer = convolution(_layer, self.network_architecture["filter4"], self.network_architecture["stride4"], self.ini_c)
         _layer = tf.nn.max_pool(_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         _layer = self.activation(_layer)
         _layer = tf.nn.dropout(_layer, _keep_prob)
         # print(_layer.shape)
         _layer = slim.flatten(_layer)
         _shape = _layer.shape.as_list()
-        _logit = full_connected(_layer, [_shape[-1], self.architecture["output"]], self.ini)
+        _logit = full_connected(_layer, [_shape[-1], self.network_architecture["output"]], self.ini)
 
         self.prediction = tf.nn.softmax(_logit)
         # Define loss function (cross entropy)
@@ -114,9 +104,15 @@ class CNN(object):
         # Accuracy
         correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.prediction, 1))
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
         # Define optimizer
         optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        self.train = slim.learning.create_train_op(self.loss, optimizer)
+        if self.max_grad_norm:
+            _var = tf.trainable_variables()
+            grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, _var), self.max_grad_norm)
+            self.train = optimizer.apply_gradients(zip(grads, _var))
+        else:
+            self.train = optimizer.minimize(self.loss)
         # saver
         self.saver = tf.train.Saver()
 
